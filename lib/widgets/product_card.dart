f944f4173f356d1d9fa3c4601f32e0a59b0c0f19
Mikayil S <sert.mikayil@gmail.com>
2019-10-21 20:41:45 +0200

    feat(widget): add InkWell widget in product card

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -5,26 +5,30 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            height: 100.0,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0),
-                image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1289&q=80'), fit: BoxFit.cover)
+      child: InkWell(
+        onTap: () { print('Pressed on product'); },
+        highlightColor: Colors.transparent,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1289&q=80'), fit: BoxFit.cover)
+              ),
             ),
-          ),
-          SizedBox(height: 10.0,),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Product title', style: TextStyle(color: Color(0XFF202020), fontSize: 12.0, fontWeight: FontWeight.bold),),
-              SizedBox(height: 5,),
-              Text('Nuance Discrètes', style: TextStyle(color: Color(0XFFC4C4C4), fontSize: 11.0),),
-            ],
-          )
-        ],
+            SizedBox(height: 10.0,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Product title', style: TextStyle(color: Color(0XFF202020), fontSize: 12.0, fontWeight: FontWeight.bold),),
+                SizedBox(height: 5,),
+                Text('Nuance Discrètes', style: TextStyle(color: Color(0XFFC4C4C4), fontSize: 11.0),),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
