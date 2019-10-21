02b5b7e7b36039f40fdda69f7cd6327f0f82aeb2
Mikayil S <sert.mikayil@gmail.com>
2019-10-21 20:33:51 +0200

    update(widget): update the trend_card widget

--- lib/widgets/trend_card.dart
+++ lib/widgets/trend_card.dart
@@ -10,33 +10,37 @@ class TrendCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () { print('Product trend pressed.'); },
-      child: AspectRatio(
-        aspectRatio: 2.5 / 2,
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 150.0,
-              width: 270.0,
-              margin: EdgeInsets.only(right: 10.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0),
-                color: Colors.grey[300],
-                image: DecorationImage(image: NetworkImage(this.image), fit: BoxFit.cover)
+    return Container(
+      margin: EdgeInsets.only(right: 10.0),
+      child: InkWell(
+        onTap: () { print('Product trend pressed.'); },
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        child: AspectRatio(
+          aspectRatio: 2.5 / 2,
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 150.0,
+                width: 270.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Colors.grey[300],
+                  image: DecorationImage(image: NetworkImage(this.image), fit: BoxFit.cover)
+                ),
               ),
-            ),
-            SizedBox(height: 10.0,),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Text(this.title.toUpperCase(), style: TextStyle(color: Color(0XFF202020), fontSize: 12.0, fontWeight: FontWeight.bold),),
-                  SizedBox(height: 5,),
-                  Text(this.subTitle, style: TextStyle(color: Color(0XFFC4C4C4), fontSize: 11.0),)
-                ],
-              ),
-            )
-          ],
+              SizedBox(height: 10.0,),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Text(this.title.toUpperCase(), style: TextStyle(color: Color(0XFF202020), fontSize: 12.0, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 5,),
+                    Text(this.subTitle, style: TextStyle(color: Color(0XFFC4C4C4), fontSize: 11.0),)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
